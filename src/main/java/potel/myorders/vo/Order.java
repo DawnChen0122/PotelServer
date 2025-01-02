5394581a1a6bd5be47bb12ae149f174deef6943d
JohnnyWu <orion_wu@yahoo.com.tw>
2025-01-02 09:40:01 +0800

    增加lombok函式庫, 把bean寫法修改 1.移除set, get 2.加上@Data

--- src/main/java/potel/myorders/vo/Order.java
+++ src/main/java/potel/myorders/vo/Order.java
@@ -1,39 +1,42 @@
 package potel.myorders.vo;
 
+import lombok.Data;
+
+@Data
 public class Order {
 	private int orderid;
 	private int memberid;
 	private String orderstate;
 	private int score;
 	private String comment;
-	public int getOrderid() {
-		return orderid;
-	}
-	public void setOrderid(int orderid) {
-		this.orderid = orderid;
-	}
-	public int getMemberid() {
-		return memberid;
-	}
-	public void setMemberid(int memberid) {
-		this.memberid = memberid;
-	}
-	public String getOrderstate() {
-		return orderstate;
-	}
-	public void setOrderstate(String orderstate) {
-		this.orderstate = orderstate;
-	}
-	public int getScore() {
-		return score;
-	}
-	public void setScore(int score) {
-		this.score = score;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+//	public int getOrderid() {
+//		return orderid;
+//	}
+//	public void setOrderid(int orderid) {
+//		this.orderid = orderid;
+//	}
+//	public int getMemberid() {
+//		return memberid;
+//	}
+//	public void setMemberid(int memberid) {
+//		this.memberid = memberid;
+//	}
+//	public String getOrderstate() {
+//		return orderstate;
+//	}
+//	public void setOrderstate(String orderstate) {
+//		this.orderstate = orderstate;
+//	}
+//	public int getScore() {
+//		return score;
+//	}
+//	public void setScore(int score) {
+//		this.score = score;
+//	}
+//	public String getComment() {
+//		return comment;
+//	}
+//	public void setComment(String comment) {
+//		this.comment = comment;
+//	}
 }
