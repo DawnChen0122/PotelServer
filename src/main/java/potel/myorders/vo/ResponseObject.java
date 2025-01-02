79f35c12de2ab9b28d2280c6482d919e7227de35
JohnnyWu <orion_wu@yahoo.com.tw>
2025-01-02 13:42:31 +0800

    更改函式庫 1.HikariCP 5.1.0->6.2.1 2.增加lombok-1.18.36 讓data bean寫起來方便

--- src/main/java/potel/myorders/vo/ResponseObject.java
+++ src/main/java/potel/myorders/vo/ResponseObject.java
@@ -1,18 +1,9 @@
 package potel.myorders.vo;
 
+import lombok.Data;
+
+@Data
 public class ResponseObject {
 	private int responsecode;
 	private String responsemessage;
-	public int getResponsecode() {
-		return responsecode;
-	}
-	public void setResponsecode(int responsecode) {
-		this.responsecode = responsecode;
-	}
-	public String getResponsemessage() {
-		return responsemessage;
-	}
-	public void setResponsemessage(String responsemessage) {
-		this.responsemessage = responsemessage;
-	}
 }
