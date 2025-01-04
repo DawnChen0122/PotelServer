390042c5b0dcdcd6eccf9b656e5e634484e15b2c
Kent <kent81577@gmail.com>
2025-01-04 14:59:00 +0800

    123

--- src/main/java/potel/petsfile/controller/PetsFileController.java
+++ src/main/java/potel/petsfile/controller/PetsFileController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import potel.petsfile.service.PetsFileService;
 import potel.petsfile.service.impl.PetsFileServiceImpl;
 import potel.petsfile.vo.PetsFile;
 
+@WebServlet("/potel/petsfile")
 public class PetsFileController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
