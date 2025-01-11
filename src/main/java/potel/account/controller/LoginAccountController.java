7815540b5432e2cbae8572ebba59c0f7e877923a
Jim <a86421@gmail.com>
2025-01-11 12:20:40 +0800

    Login

---
+++ src/main/java/potel/account/controller/LoginAccountController.java
@@ -0,0 +1,47 @@
+package potel.account.controller;
+
+import java.io.IOException;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import potel.account.service.AccountService;
+import potel.account.service.impl.AccountServiceImpl;
+import potel.account.vo.Member;
+
+@WebServlet("/member/login")
+public class LoginAccountController extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+		private AccountService accountService;
+
+		@Override
+		public void init() throws ServletException {
+			try {
+				accountService = new AccountServiceImpl();
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			req.setCharacterEncoding("UTF-8");
+			resp.setContentType("application/json");
+			resp.setCharacterEncoding("UTF-8");
+			String input = req.getParameter("INPUT");
+			String passwd = req.getParameter("PASSWORD");
+			boolean result = accountService.login(input,passwd);
+			JsonObject respBody = new JsonObject();
+			respBody.addProperty("success", result);
+			respBody.addProperty("message", result ? "正確" : "不正確");
+			resp.getWriter().write(respBody.toString());
+		}
+
+}
